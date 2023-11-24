cd09078f1fc7c9d022b51a69ebb5b88ab5395d83
Dylan Palin <dylanpalin@gmail.com>
2023-11-23 20:59:55 -0700

    completed assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
+const transpose = require('./transpose');
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const verticalJoin = letters.map(ls => ls.join(''))
+    for (v of verticalJoin) {
+        if (v.includes(word)) return true
+    }
+    const transposedLetters = transpose(letters);
+    const verticalJoinFlip = transposedLetters.map(ls => ls.join(''))
+    for (v of verticalJoinFlip) {
+        if (v.includes(word)) return true
+    }
+    return false;
 }
+
 
 module.exports = wordSearch
